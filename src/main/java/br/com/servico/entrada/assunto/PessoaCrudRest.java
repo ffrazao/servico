0c2dbc30ebe4f5175be771953cfcd24919e023ca
Fernando Frazão <ffrazao@users.noreply.github.com>
2017-05-01 17:13:55 -0300

    inclusao do jasypt-spring-boot-starter para criptografar dados de arquivos de propriedade

--- src/main/java/br/com/servico/entrada/assunto/PessoaCrudRest.java
+++ src/main/java/br/com/servico/entrada/assunto/PessoaCrudRest.java
@@ -1,9 +1,13 @@
 package br.com.servico.entrada.assunto;
 /*
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import br.com.servico.banco_dados.mysql.aterweb.modelo.pessoa.Pessoa;
 import br.com.servico.entrada._BaseCrudRest;
 import br.com.servico.transporte.assunto.ModeloFiltroDto;
 
@@ -13,6 +17,22 @@ public class PessoaCrudRest extends _BaseCrudRest<Pessoa, ModeloFiltroDto> {
 
 	public PessoaCrudRest() {
 		super("Pessoa", Pessoa.class, ModeloFiltroDto.class);
+	}
+
+
+	@RequestMapping("/mens")
+	public SseEmitter getRealTimeMessageAction(HttpServletRequest request) throws IOException {
+		// You can send message here
+		sseEmitter.send("Message #1");
+		return sseEmitter;
+	}
+
+	private SseEmitter sseEmitter = new SseEmitter();
+
+	@RequestMapping("/ola")
+	public SseEmitter ola() throws IOException {
+		sseEmitter.send("Ola!");
+		return sseEmitter;
 	}
 
 }
