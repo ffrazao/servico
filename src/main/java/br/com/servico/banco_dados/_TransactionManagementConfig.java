7bf5d6d3de106ac833da9431b1bf51515a45728a
Fernando Frazão <ffrazao@users.noreply.github.com>
2017-05-10 13:12:55 -0300

    Desativar o timeout do framework Atomikos

--- src/main/java/br/com/servico/banco_dados/_TransactionManagementConfig.java
+++ src/main/java/br/com/servico/banco_dados/_TransactionManagementConfig.java
@@ -1,5 +1,8 @@
 package br.com.servico.banco_dados;
 
+import java.util.Properties;
+
+import javax.transaction.SystemException;
 import javax.transaction.TransactionManager;
 import javax.transaction.UserTransaction;
 
@@ -11,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 
 import com.atomikos.icatch.config.UserTransactionServiceImp;
+import com.atomikos.icatch.config.imp.AbstractUserTransactionServiceFactory;
 import com.atomikos.icatch.jta.UserTransactionImp;
 import com.atomikos.icatch.jta.UserTransactionManager;
 
@@ -20,14 +24,18 @@ public class _TransactionManagementConfig {
 
 	@Bean
 	@DependsOn("userTransactionServiceImp")
-	public PlatformTransactionManager transactionManager() {
-		return new JtaTransactionManager(userTransaction(), userTransactionManager());
+	public PlatformTransactionManager transactionManager() throws SystemException {
+		JtaTransactionManager result = new JtaTransactionManager(userTransaction(), userTransactionManager());
+		
+		return result;
 	}
 
 	@Bean
 	@DependsOn("userTransactionServiceImp")
-	public UserTransaction userTransaction() {
-		return new UserTransactionImp();
+	public UserTransaction userTransaction() throws SystemException {
+		UserTransactionImp result = new UserTransactionImp();
+		
+		return result;
 	}
 
 	@Bean
@@ -36,12 +44,22 @@ public class _TransactionManagementConfig {
 		UserTransactionManager result = new UserTransactionManager();
 		result.setStartupTransactionService(false);
 		result.setForceShutdown(false);
+		
 		return result;
 	}
 
 	@Bean(name = "userTransactionServiceImp")
 	public UserTransactionServiceImp userTransactionServiceImp() {
-		return new UserTransactionServiceImp();
+		UserTransactionServiceImp result = new UserTransactionServiceImp();
+		
+		// código para desligar o recurso de timeout do framework
+		Properties properties = new Properties();
+		properties.setProperty(AbstractUserTransactionServiceFactory.MAX_TIMEOUT_PROPERTY_NAME, "0");
+		properties.setProperty(AbstractUserTransactionServiceFactory.DEFAULT_JTA_TIMEOUT_PROPERTY_NAME, "0");
+		
+		result.init(properties);
+
+		return result;
 	}
 
 }
