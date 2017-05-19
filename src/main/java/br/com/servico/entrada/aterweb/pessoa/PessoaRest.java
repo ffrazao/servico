26eb3227fb000579a398584db64f8d7c434779b7
Fernando Frazão <ffrazao@users.noreply.github.com>
2017-05-18 21:27:42 -0300

    validacao do rest de pessoa esta funcionando

--- src/main/java/br/com/servico/entrada/aterweb/pessoa/PessoaRest.java
+++ src/main/java/br/com/servico/entrada/aterweb/pessoa/PessoaRest.java
@@ -1,12 +1,13 @@
 package br.com.servico.entrada.aterweb.pessoa;
 
-import java.security.Principal;
 import java.util.Collection;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,14 +23,14 @@ public class PessoaRest extends _BaseRest {
 
 	public PessoaRest() {
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@RequestMapping(method = RequestMethod.POST, path = "/teste")
-	public ResponseEntity<Collection<PessoaListaDto>> filtrar(@RequestBody(required = true) PessoaFiltroDto filtro, Principal usuario, BindingResult bindingResult) throws Exception {
-		if (bindingResult.hasErrors()) {
-			throw new Exception(bindingResult.getNestedPath());
+	public ResponseEntity<Collection<PessoaListaDto>> filtrar(@Valid @RequestBody(required = true) PessoaFiltroDto filtro, Errors errors) throws Exception {
+		if (errors.hasErrors()) {
+			throw new Exception(errors.getAllErrors().stream().map(x -> x.getDefaultMessage()).collect(Collectors.joining(",")));
 		}
-		return new ResponseEntity<Collection<PessoaListaDto>>((Collection<PessoaListaDto>) getFacadeBo().executarSomenteLeitura("AterwebPessoaFiltrarCh", filtro, usuario), HttpStatus.OK);
+		return new ResponseEntity<Collection<PessoaListaDto>>((Collection<PessoaListaDto>) getFacadeBo().executarSomenteLeitura("AterwebPessoaFiltrarCh", filtro, null), HttpStatus.OK);
 	}
 
 }
