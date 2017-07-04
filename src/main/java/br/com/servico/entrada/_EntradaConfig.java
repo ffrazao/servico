c73d16031c4aa0743a2887ab187fdc7134944ce2
Fernando Frazão <ffrazao@users.noreply.github.com>
2017-07-03 23:25:24 -0300

    inclusao de mapeamento para paginas jsp (mvc habilitado)

--- src/main/java/br/com/servico/entrada/_EntradaConfig.java
+++ src/main/java/br/com/servico/entrada/_EntradaConfig.java
@@ -3,21 +3,59 @@ package br.com.servico.entrada;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 public class _EntradaConfig extends WebMvcConfigurerAdapter {
 
+	// @Bean
+	// public CookieLocaleResolver localeResolver() {
+	// CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+	// localeResolver.setDefaultLocale(new Locale("pt", "BR"));
+	// localeResolver.setCookieMaxAge(3600);
+	// localeResolver.setCookieName("padrao-locale-cookie");
+	// return localeResolver;
+	// }
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeInterceptor());
+	}
+
+	/**
+	 * redirect a user to the welcome page when he visits tha app without a
+	 * destination url.
+	 */
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index.html");
+		registry.addViewController("/login").setViewName("login");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		super.addViewControllers(registry);
+	}
+
 	@Bean
-	public ReloadableResourceBundleMessageSource messageSource() {
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("classpath:messages");
-		messageSource.setDefaultEncoding("UTF-8");
-		return messageSource;
+	public LocaleChangeInterceptor localeInterceptor() {
+		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+		interceptor.setParamName("lang");
+		return interceptor;
+	}
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/visao/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
 	}
 
 	// @Bean
@@ -30,14 +68,11 @@ public class _EntradaConfig extends WebMvcConfigurerAdapter {
 	// }
 
 	@Bean
-	public LocaleChangeInterceptor localeInterceptor() {
-		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-		interceptor.setParamName("lang");
-		return interceptor;
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
 	}
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(localeInterceptor());
-	}
 }
